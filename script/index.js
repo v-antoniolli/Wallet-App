f87c0f489a9822f3fc563b73b8b65dbae5ac0f39
v-antoniolli <125698945+v-antoniolli@users.noreply.github.com>
2023-03-08 23:37:37 -0300

    +1

--- script/index.js
+++ script/index.js
@@ -1,3 +1,13 @@
+const validateUser = async (email) => {
+  try {
+    const result = await fetch(`https://mp-wallet-app-api.herokuapp.com/users?email=${email}`)
+    const user = await result.json()
+    return user
+  } catch (error) {
+    return {error}
+  }
+}
+
 const onClickLogin = () => {
   const email = document.getElementById("input-email").value;
   if (email.length < 5 || !email.includes("@")) {
