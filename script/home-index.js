82232672252a15f504b97a97e73297f768f4c35d
v-antoniolli <125698945+v-antoniolli@users.noreply.github.com>
2023-03-10 00:19:04 -0300

    elementos html

--- script/home-index.js
+++ script/home-index.js
@@ -1,4 +1,34 @@
+const onLoadUserInfo = () => {
+  const email = localStorage.getItem("@walletApp:userEmail")
+  const name = localStorage.getItem("@WalletApp:userName")
+
+  const navBarUserInfo = document.getElementById("navbar-user-container")
+  const navBarUserAvatar = document.getElementById("navbar-user-avatar")
+
+  //add user email
+  const emailElement = document.createElement("p")
+  const emailText = document.createTextNode(email)
+  emailElement.appendChild(emailText)
+  navBarUserInfo.appendChild(emailElement)
+
+  //add logout link (a)
+  const logoutElement = document.createElement("a")
+  const logoutText = document.createTextNode("sair")
+  logoutElement.appendChild(logoutText)
+  navBarUserInfo.appendChild(logoutElement)
+
+  // add user first letter name
+  const nameElement = document.createElement("h3")
+  const nameText = document.createTextNode(name.charAt(0))
+  nameElement.appendChild(nameText)
+  navBarUserAvatar.appendChild(nameElement)
+}
+  
+
+
+
+
+
 window.onload = () => {
-  const email = localStorage.getItem("@walletApp:userEmail");
-  console.log(email)
+  onLoadUserInfo()
 }
